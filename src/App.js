829380f67ec4e68ffecebff7be5d3f12bc184fa2
Teddy <edmondteddy@hotmail.fr>
2022-12-04 19:28:48 +0100

    prep for the new component
    card component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from './components/Navigation/NavBar'
 import Filter from './components/Filter/Filter'
+import Card from './components/Card/Card'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <NavBar />
         <Filter />
       </header>
+      <Card />
     </div>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+  width: 10em;
+  height: 10em;
+  background-color: blueviolet;
+`
+
+export default function Card() {
+  return <CardContainer>hello world</CardContainer>
+}
